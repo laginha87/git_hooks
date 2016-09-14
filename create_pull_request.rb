6f8b5b800e80bc5e52a89eb8007b50b28b3b5dc6
chick-chains <filipe.lp.correia@gmail.com>
2016-09-14 21:35:32 +0100

    Finito

--- create_pull_request.rb
+++ create_pull_request.rb
@@ -1,7 +1,15 @@
-#!/bin/ruby
+#!/usr/bin/ruby
 
-# this hook requires gem git-pulls
-Thread.new do
-  puts "FFS"
+current_branch = %x(git rev-parse --abbrev-ref HEAD).sub("\n","")
+repo_url = %x(git config --get remote.origin.url).sub(".git\n", '')
+%x(git pulls list)
+pull_requests = %x(git pulls list)
+possible_pull_request = pull_requests.split("\n").grep(Regexp.new(current_branch))
+has_pull_request = !possible_pull_request.empty?
+
+if(has_pull_request)
+  pull_request_num = possible_pull_request.first.split(' ').first
+  puts "Current Pull Request is: #{repo_url}/pull/#{pull_request_num}"
+else
+  puts "Create a New Pull request: #{repo_url}/compare/#{current_branch}"
 end
-# while(File.exists?("../"))
